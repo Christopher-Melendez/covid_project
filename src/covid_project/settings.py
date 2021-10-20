6dc612df3a70be4b7eb734e32635415854ae3b54
yangtammy <35717440+yangtammy@users.noreply.github.com>
2021-10-19 21:45:34 -0400

    database connected

--- src/covid_project/settings.py
+++ src/covid_project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'covid_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        #replace with database name
+        'NAME' : 'covidproject',
+        'USER': 'postgres',
+        'PASSWORD': '2314',
+        'HOST': 'localhost'
     }
 }
 
