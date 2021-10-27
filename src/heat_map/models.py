e7d87eafdd81a2dcb375bb3a95b0249ddef26035
shohcarroll <Shohtaro.Carroll42@myhunter.cuny.edu>
2021-10-26 22:52:01 -0400

    Update models.py

--- src/heat_map/models.py
+++ src/heat_map/models.py
@@ -18,8 +18,28 @@ class Map(models.Model):
         choices = MAP_CHOICES,
         default = COVID_CASES,
     )
+class Map(models,Model):
+    c_income_id = models.FloadField(default=0.0)
+    perc_0-10 = models.FloatField(default=0.0)
+    perc_10-15 = models.FloatField(default=0.0)
+    perc_15-20 = models.FloatField(default=0.0)
     perc_20-25 =  models.FloatField(default=0.0)
+    perc_25-30 =  models.FloatField(default=0.0)
+    perc_30-35 =  models.FloatField(default=0.0)
+    perc_35-40 =  models.FloatField(default=0.0)
+    perc_40-45 =  models.FloatField(default=0.0)
+    perc_45-50 =  models.FloatField(default=0.0)
+    perc_50-60 =  models.FloatField(default=0.0)
+    perc_60-75 =  models.FloatField(default=0.0)
+    perc_75-100 =  models.FloatField(default=0.0)
+    perc_100-125 =  models.FloatField(default=0.0)
+    perc_125-150 =  models.FloatField(default=0.0)
+    perc_150-200 =  models.FloatField(default=0.0)
+    perc_200-up =  models.FloatField(default=0.0)
+    med_income =  models.FloatField(default=0.0)
 
+    
+    
 
     #Run a select query to get database views in the init file to join the 3 tables (Data/County/Latlong) & Then Populate Map using those..
     #
