8fd53421ebe0069841c9dde1c2781bd57366c0af
x||Chris||x <chrismelendez2000@gmail.com>
2021-10-27 00:04:19 -0400

    PUT MAP IN PLACE

--- src/heat_map/maps.py
+++ src/heat_map/maps.py
@@ -2,7 +2,7 @@ import folium
 import pandas as pd
 import json
 import os
-import geopandas as gpd
+#import geopandas as gpd
 
 
 #TEST COMMENT NEW THING DONE
@@ -20,15 +20,18 @@ def maps(map_choice):
 
     #Result Set to store SQL query Wtih all rows.. run for loop to iterate through results and populate the overlays..
 
-    counties = os.path.join('data', 'ny_counties.json')
-    covid_data = os.path.join('data', 'COVID_CASES.csv')
+    #counties = os.path.join('data', 'ny_counties.json')
+    #covid_data = os.path.join('data', 'COVID_CASES.csv')
+    counties = 'heat_map/data/ny_counties.json'
+    covid_data = 'heat_map/data/COVID_CASES.csv'
     county_data = pd.read_csv(covid_data)
 
     m = folium.Map(location=[43.2994, -74.2179], zoom_start=7)
     folium.TileLayer('CartoDB positron',name="Light Map",control=False).add_to(m)
 
-
+    print("MAKING MAP")
     if map_choice == 'COVID-19 Cases':
+        print("ALMOST THERE")
         m.choropleth(
             geo_data=counties,
             name='choropleth',
@@ -38,31 +41,33 @@ def maps(map_choice):
             fill_color='OrRd',
             fill_opacity=0.7,
             line_opacity=0.2,
-            legend_name='Percent Positive'
+            legend_name='Percent Positive',
+            
         )
+        print("CHLORO")
 
         folium.LayerControl().add_to(m)
 
-    elif map_choice == 'COVID-19 Deaths':
-        folium.Marker(
-            [43.272600, -74.458500],
-            popup='<strong>Location Two</strong>',
-            tooltip=tooltip,
-            icon=folium.Icon(icon='cloud')
-        ).add_to(m)
-    elif map_choice == 'Median Income':
-        folium.Marker(
-            [43.332600, -74.228500], 
-            popup='<strong>Location Three</strong>',
-            tooltip=tooltip,
-            icon=folium.Icon(color='purple')
-        ).add_to(m)
-    elif map_choice == 'Insurance Coverage':
-        folium.Marker([43.362600, -74.208500], 
-            popup='<strong>Location Four</strong>',
-            tooltip=tooltip,
-            icon=folium.Icon(color='green', icon='leaf')
-        ).add_to(m)
+    # elif map_choice == 'COVID-19 Deaths':
+    #     folium.Marker(
+    #         [43.272600, -74.458500],
+    #         popup='<strong>Location Two</strong>',
+    #         tooltip=tooltip,
+    #         icon=folium.Icon(icon='cloud')
+    #     ).add_to(m)
+    # elif map_choice == 'Median Income':
+    #     folium.Marker(
+    #         [43.332600, -74.228500], 
+    #         popup='<strong>Location Three</strong>',
+    #         tooltip=tooltip,
+    #         icon=folium.Icon(color='purple')
+    #     ).add_to(m)
+    # elif map_choice == 'Insurance Coverage':
+    #     folium.Marker([43.362600, -74.208500], 
+    #         popup='<strong>Location Four</strong>',
+    #         tooltip=tooltip,
+    #         icon=folium.Icon(color='green', icon='leaf')
+    #     ).add_to(m)
     else:
         print("WELP ~.~")
     
@@ -91,6 +96,7 @@ def maps(map_choice):
     #Generate map
     file_path = 'heat_map/temp/map.html'
     m.save(file_path)
+    print("SAVE")
 
     with open(file_path, 'r') as file:
         map_html_text = file.read().replace('\n', '')
