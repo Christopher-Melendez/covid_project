5e9c40a682118aaeffab72104025b54e81a46960
x||Chris||x <chrismelendez2000@gmail.com>
2021-11-02 23:23:33 -0400

    model to dataframe

--- src/heat_map/maps.py
+++ src/heat_map/maps.py
@@ -11,6 +11,19 @@ from tables.models import covid_cases
 
 #TEST COMMENT NEW THING DONE
 
+def model_to_df(model, columns):
+        returning_list = []
+        data_dict = {}
+        data_rows = model.objects.all().count()
+        for i in columns:
+            temp_list = []
+            for j in range(data_rows):
+                row = model.objects.get(id=(j+1))
+                temp_list.append(getattr(row, i))
+            returning_list.append(temp_list)
+            data_dict[i] = temp_list
+        data_frame = pd.DataFrame(data=data_dict)
+        return data_frame
 
 def maps(map_choice):
 
@@ -31,31 +44,38 @@ def maps(map_choice):
     #county_data = pd.read_csv(covid_data)
     
     #data_rows = covid_cases.objects.filter()
-    # def model_to_df(model, columns):
-    #     list_1 = []
-    #     list_2 = []
-    #     data_rows = model.objects.all().count()
-
-    #     for i in range(data_rows):
-    #         row = model.objects.get(id=(i+1))
-    #         list_1.append(row.columns[1])
-    #         list_2.append(row.columns[2])
-    #     data_dict = {columns[1]: list_1, columns[2]: list_2}
-    #     data_frame = pd.DataFrame(data=data_dict)
-    #     return data_frame
-
-
-    data_rows = covid_cases.objects.all().count()
-    list_1 = []
-    list_2 = []
     
-    for i in range(data_rows):
-        row = covid_cases.objects.get(id=(i+1))
-        list_1.append(row.county)
-        list_2.append(row.C_Pos_C_Test)
+
+        
+            
+
+
+        # list_1 = []
+        # list_2 = []
+        # for i in range(data_rows):
+        #     row = model.objects.get(id=(i+1))
+        #     for i in range(columns):
+        #         temp_list = []
+        #         temp_list.append(getattr(row, columns[i]))
+        #     list_1.append(getattr(row, columns[0]))
+        #     list_2.append(getattr(row, columns[1]))
+        # data_dict = {columns[0]: list_1, columns[1]: list_2}
+        # data_frame = pd.DataFrame(data=data_dict)
+        # print("Worked?")
+        # return data_frame
+
+
+    # data_rows = covid_cases.objects.all().count()
+    # list_1 = []
+    # list_2 = []
     
-    data_dict = {'County': list_1, 'C_POS/C_TEST': list_2}
-    data_frame = pd.DataFrame(data=data_dict)
+    # for i in range(data_rows):
+    #     row = covid_cases.objects.get(id=(i+1))
+    #     list_1.append(row.county)
+    #     list_2.append(row.C_Pos_C_Test)
+    
+    # data_dict = {'County': list_1, 'C_POS/C_TEST': list_2}
+    # data_frame = pd.DataFrame(data=data_dict)
 
     m = folium.Map(location=[43.2994, -74.2179], zoom_start=7)
     folium.TileLayer('CartoDB positron',name="Light Map",control=False).add_to(m)
@@ -64,8 +84,8 @@ def maps(map_choice):
         m.choropleth(
             geo_data=counties,
             name='choropleth',
-            data= model_to_df(covid_cases, ['County', 'C_POS/C_TEST']),
-            columns=['County', 'C_POS/C_TEST'],
+            data= model_to_df(covid_cases, ['county', 'C_Pos_C_Test']),
+            columns=['county', 'C_Pos_C_Test'],
             key_on='feature.properties.name',
             fill_color='OrRd',
             fill_opacity=0.7,
