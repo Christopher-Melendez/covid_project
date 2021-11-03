49ad26b19adb10f1d1f0b05a442ec4bd4c5be30d
x||Chris||x <chrismelendez2000@gmail.com>
2021-11-02 23:49:41 -0400

    Cleaning Up maps.py

--- src/heat_map/maps.py
+++ src/heat_map/maps.py
@@ -9,8 +9,7 @@ from tables.models import covid_cases
 #import geopandas as gpd
 
 
-#TEST COMMENT NEW THING DONE
-
+#Import Data from Data Models and Convert to Pandas Data Frame
 def model_to_df(model, columns):
         returning_list = []
         data_dict = {}
@@ -26,61 +25,20 @@ def model_to_df(model, columns):
         return data_frame
 
 def maps(map_choice):
-
-    #Create map object, N&E positive, S&W negative
-
-    #Global tooltip
-    tooltip = 'Click for More Info'
-
-    #Create markers
-
-
-    #Result Set to store SQL query Wtih all rows.. run for loop to iterate through results and populate the overlays..
-
-    #counties = os.path.join('data', 'ny_counties.json')
-    #covid_data = os.path.join('data', 'COVID_CASES.csv')
+    #Temp Stuff Till Database Finalized...
     counties = 'heat_map/data/ny_counties.json'
-    #covid_data = 'heat_map/data/COVID_CASES.csv'
-    #county_data = pd.read_csv(covid_data)
+    covid_deaths = 'heat_map/data/COVID_DEATHS.csv'
+    county_data = pd.read_csv(covid_deaths)
     
-    #data_rows = covid_cases.objects.filter()
     
-
-        
-            
-
-
-        # list_1 = []
-        # list_2 = []
-        # for i in range(data_rows):
-        #     row = model.objects.get(id=(i+1))
-        #     for i in range(columns):
-        #         temp_list = []
-        #         temp_list.append(getattr(row, columns[i]))
-        #     list_1.append(getattr(row, columns[0]))
-        #     list_2.append(getattr(row, columns[1]))
-        # data_dict = {columns[0]: list_1, columns[1]: list_2}
-        # data_frame = pd.DataFrame(data=data_dict)
-        # print("Worked?")
-        # return data_frame
-
-
-    # data_rows = covid_cases.objects.all().count()
-    # list_1 = []
-    # list_2 = []
     
-    # for i in range(data_rows):
-    #     row = covid_cases.objects.get(id=(i+1))
-    #     list_1.append(row.county)
-    #     list_2.append(row.C_Pos_C_Test)
-    
-    # data_dict = {'County': list_1, 'C_POS/C_TEST': list_2}
-    # data_frame = pd.DataFrame(data=data_dict)
-
+    #Initializing Folium Map Cenetered in Central New York
+    tooltip = 'Click for More Info'
     m = folium.Map(location=[43.2994, -74.2179], zoom_start=7)
     folium.TileLayer('CartoDB positron',name="Light Map",control=False).add_to(m)
 
     if map_choice == 'COVID-19 Cases':
+        #Ryan Created Chloropleth Map For Percent Stat of Positive Covid Cases / Total Tests Performed by County
         m.choropleth(
             geo_data=counties,
             name='choropleth',
@@ -91,15 +49,11 @@ def maps(map_choice):
             fill_opacity=0.7,
             line_opacity=0.2,
             legend_name='Percent Positive',
-            
         )
-
         folium.LayerControl().add_to(m)
 
     elif map_choice == 'COVID-19 Deaths':
-        covid_deaths = 'heat_map/data/COVID_DEATHS.csv'
-        county_data = pd.read_csv(covid_deaths)
-
+        #Ryan Created Chloropleth Map For Deaths Per 100k By County
         m.choropleth(
             geo_data=counties,
             name='choropleth',
@@ -129,30 +83,7 @@ def maps(map_choice):
     else:
         pass
     
-    # folium.Marker([lat_1, long_1], 
-    #             popup='<strong>Location One</strong>',
-    #             tooltip=tooltip).add_to(map),
-    # folium.Marker([43.272600, -74.458500], 
-    #             popup='<strong>Location Two</strong>',
-    #             tooltip=tooltip,
-    #             icon=folium.Icon(icon='cloud')).add_to(map),
-    # folium.Marker([43.332600, -74.228500], 
-    #             popup='<strong>Location Three</strong>',
-    #             tooltip=tooltip,
-    #             icon=folium.Icon(color='purple')).add_to(map),
-    # folium.Marker([43.362600, -74.208500], 
-    #             popup='<strong>Location Four</strong>',
-    #             tooltip=tooltip,
-    #             icon=folium.Icon(color='green', icon='leaf')).add_to(map),
-
-
-    
- 
-
-
-
-    #Generate map
-    
+    #Generate map as HTML Text String and return
     map_html_text = m.get_root().render()
 
     return map_html_text
