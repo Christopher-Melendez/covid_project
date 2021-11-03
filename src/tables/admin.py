5b591a7cf29e7731c07a1cf87bafef4e63b089b3
x||Chris||x <chrismelendez2000@gmail.com>
2021-11-02 22:38:04 -0400

    FIXED DB MIGRATIONS BOOM

--- src/tables/admin.py
+++ src/tables/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import income_id
-admin.site.register(income_id)
+# from .models import income_id
+# admin.site.register(income_id)
 
-from .models import ethnicity_id
-admin.site.register(ethnicity_id)
+# from .models import ethnicity_id
+# admin.site.register(ethnicity_id)
 
-from .models import covid_cases
-admin.site.register(covid_cases)
+# from .models import covid_cases
+# admin.site.register(covid_cases)
 
-from .models import Age
-admin.site.register(Age)
+# from .models import Age
+# admin.site.register(Age)
 
 
