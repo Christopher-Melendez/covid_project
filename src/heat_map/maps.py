8894a88cf1c653e50eb9faa7f7a8bafaa187d219
x||Chris||x <chrismelendez2000@gmail.com>
2021-11-30 21:25:15 -0500

    Made the Maps Work From Database

--- src/heat_map/maps.py
+++ src/heat_map/maps.py
@@ -1,28 +1,11 @@
 import folium
 import pandas as pd
-import json
-import os
-import tables
 from django.db.models import F
-from tables.models import covid_cases, covid_deaths #, labor_stats, health_stats
- 
-#import geopandas as gpd
-
+from tables.models import covid_cases, covid_deaths, labor_stats, health_stats
 
 #Import Data from Data Models and Convert to Pandas Data Frame
-def model_to_df(model, columns):
-        returning_list = []
-        data_dict = {}
-        data_rows = model.objects.all().count()
-        for i in columns:
-            temp_list = []
-            for j in range(data_rows):
-                row = model.objects.get(id=(j+1))
-                temp_list.append(getattr(row, i))
-            returning_list.append(temp_list)
-            data_dict[i] = temp_list
-        data_frame = pd.DataFrame(data=data_dict)
-        return data_frame
+from tables.tables import model_to_df
+
 
 def maps(map_choice):
     #Temp Stuff Till Database Finalized...
@@ -64,16 +47,12 @@ def maps(map_choice):
         )
         folium.LayerControl().add_to(m)
 
-    elif map_choice == 'Median Income':
-        median_data = 'heat_map/data/labor_stats.csv'
-        inc_data = pd.read_csv(median_data) 
+    elif map_choice == 'Median Income': 
         m.choropleth(
             geo_data=counties,
             name='choropleth',
-            data = inc_data,
-            # data= model_to_df(labor_info, ['county', 'median_income']),
-            #Temp columns.
-            columns=['County', 'median_income'],
+            data= model_to_df(labor_stats, ['county', 'median_income']),
+            columns=['county', 'median_income'],
             key_on='feature.properties.name',
             fill_color='OrRd',
             fill_opacity=0.7,
@@ -83,14 +62,11 @@ def maps(map_choice):
         folium.LayerControl().add_to(m)
 
     elif map_choice == 'College Education':
-      college_data = 'heat_map/data/labor_stats.csv'
-      education_data = pd.read_csv(college_data) 
       m.choropleth(
           geo_data=counties,
           name='choropleth',
-          data = education_data,
-          # data= model_to_df(labor_info, ['county', 'percent_college']),
-          columns=['County', 'percent_college'],
+          data= model_to_df(labor_stats, ['county', 'percent_college']),
+          columns=['county', 'percent_college'],
           key_on='feature.properties.name',
           fill_color='OrRd',
           fill_opacity=0.7,
@@ -100,14 +76,11 @@ def maps(map_choice):
       folium.LayerControl().add_to(m)
 
     elif map_choice == 'Unemployment':
-      unemploy_data = 'heat_map/data/labor_stats.csv'
-      labor_data = pd.read_csv(unemploy_data) 
       m.choropleth(
           geo_data=counties,
           name='choropleth',
-          data = labor_data,
-          # data= model_to_df(labor_info, ['county', 'percent_unemployed']),
-          columns=['County', 'percent_unemployed'],
+          data= model_to_df(labor_stats, ['county', 'percent_unemployed']),
+          columns=['county', 'percent_unemployed'],
           key_on='feature.properties.name',
           fill_color='OrRd',
           fill_opacity=0.7,
@@ -116,15 +89,12 @@ def maps(map_choice):
       )
       folium.LayerControl().add_to(m)
 
-    elif map_choice == 'Poverty':
-      poverty_data = 'heat_map/data/labor_stats.csv'
-      status_data = pd.read_csv(poverty_data) 
+    elif map_choice == 'Poverty': 
       m.choropleth(
           geo_data=counties,
           name='choropleth',
-          data = status_data,
-          # data= model_to_df(labor_info, ['county', 'percent_poverty']),
-          columns=['County', 'percent_poverty'],
+          data= model_to_df(labor_stats, ['county', 'percent_poverty']),
+          columns=['county', 'percent_poverty'],
           key_on='feature.properties.name',
           fill_color='OrRd',
           fill_opacity=0.7,
@@ -134,14 +104,11 @@ def maps(map_choice):
       folium.LayerControl().add_to(m)
 
     elif map_choice == 'Insurance Coverage':
-        insurance_data = 'heat_map/data/insurance_coverage.csv'
-        coverage_data = pd.read_csv(insurance_data) 
         m.choropleth(
             geo_data=counties,
             name='choropleth',
-            data = coverage_data,
-            # data= model_to_df(health_stats, ['county', 'percent_insured']),
-            columns=['County', 'percent_insured'],
+            data= model_to_df(health_stats, ['county', 'percent_insured']),
+            columns=['county', 'percent_insured'],
             key_on='feature.properties.name',
             fill_color='OrRd',
             fill_opacity=0.7,
--- src/tables/views.py
+++ src/tables/views.py
@@ -6,12 +6,18 @@ from .tables import make_table, model_to_df
 # Create your views here.
 
 def tables_view(request, *args, **kewargs):
-   
-   table_choice_in = request.POST.get('table-choice')
-   
-   web_context = {
-       'table_choice': table_choice_in,
-       'table_html': make_table(table_choice_in),
-   }
-      
-   return render(request, 'tables.html', web_context)
+    #Read Submitted Post Variable From Select Form
+    table_choice_in = request.POST.get('table-choice')
+
+    #Apply Default Case
+    if str(table_choice_in) == 'None':
+        table_choice_in = 'COVID-19 Cases'
+        
+    #Update Context Variable for Template
+    web_context = {
+        'table_choice': table_choice_in,
+        #Call Make Table With Given Choice to Create HTML Text for Tables
+        'table_html': make_table(table_choice_in),
+    }
+        
+    return render(request, 'tables.html', web_context)
