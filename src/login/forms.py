e14111337e8b4f112adfad71595c40b543634a05
x||Chris||x <chrismelendez2000@gmail.com>
2021-12-06 21:48:40 -0500

    Partial Update to Accounts

--- src/login/forms.py
+++ src/login/forms.py
@@ -1,6 +1,10 @@
 from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
+from login.models import Profile
+
+from login.models import Profile
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Please Provide a Valid Email Address.')
@@ -9,4 +13,8 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2', ) 
 
-   
+
+class UpdateProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar_img']
